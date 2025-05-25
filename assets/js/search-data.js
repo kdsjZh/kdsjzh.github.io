439c4cd2c3f829ca0991cc920c3ee60493dc564f
kdsjZh <35988108+kdsjZh@users.noreply.github.com>
2025-05-25 09:32:39 +0000

    Deploying to gh-pages from @ kdsjZh/kdsjzh.github.io@b0b966954e236596ce65599a0bf55cd86adeab61 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-security",
+          title: "security",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/security/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
